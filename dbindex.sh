fd119910c867593d14795e6f3936267e0b215c28
msergei.dev <msergeidev@gmail.com>
2024-09-21 23:29:09 +0300

    upd config

--- dbindex.sh
+++ dbindex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec cloud sh -c "apk update && apk add sudo && sudo -u www-data php /var/www/html/occ db:add-missing-indices"
+docker compose exec -u 33 cloud sh -c "php /var/www/html/occ db:add-missing-indices"
