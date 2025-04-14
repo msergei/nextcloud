e300f5b7742a24d9c8bf22b5b6099a0591a4a29a
Sergei Mironov <smironov@iponweb.net>
2025-04-14 18:02:26 +0300

    new config

--- maintenance.sh
+++ maintenance.sh
@@ -2,3 +2,5 @@
 
 docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ maintenance:repair --include-expensive"
 docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ db:add-missing-indices"
+docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ config:system:set trusted_proxies 0 --value='172.20.0.0/16"
+docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ config:system:set forwarded_for_headers --value='[\"HTTP_X_FORWARDED_FOR\"]'"
