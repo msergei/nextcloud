6fbe04720cd2cf57ac5fa567e00b9703d3a7ad34
msergei <46129167+msergei@users.noreply.github.com>
2025-06-22 15:35:50 +0300

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -4,3 +4,5 @@ docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ maintenance:rep
 docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ db:add-missing-indices"
 docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ config:system:set trusted_proxies 0 --value='10.20.0.0/16"
 docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ config:system:set forwarded_for_headers --value='[\"HTTP_X_FORWARDED_FOR\"]'"
+docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ config:system:set forwarded_for_headers 0 --value='HTTP_X_FORWARDED_PROTO'"
+docker compose run --rm -u 82 cloud sh -c "php /var/www/html/occ config:system:set overwriteprotocol --value='https'"
